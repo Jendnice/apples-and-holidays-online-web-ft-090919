3c76cce8015cbbaabdb8ac32d1cbe8da0ce1756c
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-14 20:12:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, details|
    puts "#{season.to_s.capitalize}:"
      details.each do |holiday, supplies|
-   puts "  #{holiday.to_s.split("_").map do { |word| word.capitalize }.join(" ") }: #{supplies.join(", ")}" 
-   end
+   puts "  #{holiday.to_s.split("_").map  { |word| word.capitalize }.join(" ") }: #{supplies.join(", ")}" 
   end
  end 
 end
