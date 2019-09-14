8ca9eb2caf5fec1d0b33e54313d523cfb89f4d9b
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-14 20:32:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
      if supplies.include?("BBQ")
        holiday
      else
-       holiday.remove
+       holiday(supplies).delete 
      end
      end 
   end.flatten
